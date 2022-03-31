a491d5669b4c8721c45ea5db921a3cd22d99b272
Leisdon <Carlo_Leisd618@ISD11>
2022-03-31 15:34:11 -0500

    idk if this will work

--- src/main/java/frc/robot/Collector.java
+++ src/main/java/frc/robot/Collector.java
@@ -1,5 +1,8 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.motorcontrol.Victor;
+
 public class Collector {
-    
+    private Victor collector_left = new Victor(98);
+    private Victor collector_right = new Victor(99);
 }
--- src/main/java/frc/robot/Shooter.java
+++ src/main/java/frc/robot/Shooter.java
@@ -1,5 +1,9 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.motorcontrol.Victor;
+
 public class Shooter {
     
+private Victor shooter = new Victor(200);
+
 }
